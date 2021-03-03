68a97694bf03c089910f496fd46bf8ca319caed7
luckmer <woszczo04@gmail.com>
2021-03-03 22:07:30 +0100

    exports

---
+++ src/exports/Index.js
@@ -0,0 +1,8 @@
+export { default as NavBar } from "../components/NavBar";
+export { default as Calendar } from "../views/Calendar";
+export { default as ControlPanel } from "../views/ControlPanel";
+export { default as MapDays } from "../views/MapDays";
+export { default as CreateNewData } from "../views/CreateNewData";
+export { default as CurrentDate } from "../components/CurrentDate";
+export { default as Details } from "../views/Details";
+export { default as Back } from "../views/404";
