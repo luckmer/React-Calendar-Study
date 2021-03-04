4de5c9d1f964bb50585eb75a5184b05d66ba8b79
luckmer <woszczo04@gmail.com>
2021-03-04 22:10:47 +0100

    get current date

---
+++ src/components/CurrentDate.js
@@ -0,0 +1,8 @@
+const CurrentDate = () => {
+    const date = new Date();
+    let todayMonth = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return { todayMonth, year };
+};
+
+export default CurrentDate;
