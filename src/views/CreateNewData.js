ffdc374f6d7dddfe75a78950ed9a8454a737ffc8
luckmer <woszczo04@gmail.com>
2021-03-04 22:13:17 +0100

    create new task

--- src/views/CreateNewData.js
+++ src/views/CreateNewData.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import { useState } from "react";
+import { stateData, setDataForCurrentDayData } from "../reducers/DataSlice";
+import { Button, Form } from "../styles/views/CreateNewData.Style";
+import { useSelector, useDispatch } from "react-redux";
+import { Userdata } from "../reducers/ToDoSlice";
+import { AddTodo } from "../reducers/ToDoSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
-function CreateNewData() {
+function CreateNewData({ setOpen, open }) {
+    const [filter, setFilter] = useState({
+        text: "",
+    });
+    const setDataForCurrentDay = useSelector(setDataForCurrentDayData);
+    const state = useSelector(stateData);
+    const data = useSelector(Userdata);
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+        setFilter({ ...filter, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const {
+            currentUserClickedDay,
+            currentUserClickedMonth,
+            currentUserClickedYear,
+            ID,
+        } = setDataForCurrentDay;
+        if (filter.text && filter.text !== "" && filter.text.length > 1) {
+            const newData = {
+                filter,
+                id: ID,
+                description: "",
+                colorType: "#f00049",
+                day: currentUserClickedDay,
+                month: currentUserClickedMonth,
+                year: currentUserClickedYear,
+                _id: nanoid(),
+            };
+            const result = data.concat(newData);
+            dispatch(AddTodo(result));
+            setFilter("");
+            setOpen(!open);
+        }
+    };
     return (
         <div>
-            
+            <Form onSubmit={handleSubmit}>
+                {state ? state : ""}
+                <input type='text' name='text' onChange={handleChange} />
+                <Button type='submit'>Submit</Button>
+            </Form>
         </div>
-    )
+    );
 }
 
-export default CreateNewData
+export default CreateNewData;
