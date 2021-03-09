60c8c91f3603efbbaf3448167a9d0007ec35051c
luckmer <59181304+luckmer@users.noreply.github.com>
2021-03-09 15:55:25 +0100

    mobile version

--- src/styles/views/CreateNewData.Style.js
+++ src/styles/views/CreateNewData.Style.js
@@ -1,14 +1,29 @@
 import styled from "styled-components";
 
-export const Form = styled.form`
+export const Article = styled.div`
+    z-index: 999;
     display: flex;
     color: #fff;
-
-    margin: 0 10px 0 10px;
-    z-index: 999;
     flex-direction: column;
-    max-width: 100%;
     height: 100%;
+    max-width: 100%;
+    margin: 0 10px 0 10px;
+    @media screen and (max-width: 965px){
+        flex-direction: column;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+`
+
+export const Form = styled.form`
+@media screen and (max-width: 965px){
+    flex-direction: column;
+    align-items: center;
+    display: flex;
+    margin-bottom: 8vh;
+    }
     input {
         color: #ffff;
         margin-top: 10px;
@@ -19,11 +34,15 @@ export const Form = styled.form`
         box-sizing: border-box;
         border-radius: 3px;
     }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 `;
 
 export const Button = styled.button`
     width: 100px;
     height: 21px;
+    cursor: pointer;
     background-color: #21242d;
     color: #fff;
     border: 1px solid #444750;
