f485ac03db951c19c88e5a84454c0577d9004bc5
luckmer <woszczo04@gmail.com>
2021-05-18 10:49:07 +0200

    tests friendly

--- src/views/Calendar/DataControl.js
+++ src/views/Calendar/DataControl.js
@@ -1,13 +1,10 @@
-export const DataControl = (state) => {
-  let Month = state.todayMonth,
-    Year = state.todayYear;
-
-  const DateS = state.todayDay;
+export const DataControl = (Month, Year, DateS) => {
   const todayFull = `${DateS <= 9 ? "0" + DateS : DateS}.${
     Month + 1 <= 9 ? "0" + Month : Month
   }.${Year}`;
 
   let firstDay = new Date(Year, Month, 1).getDay();
   let daysInMonth = new Date(Year, Month, 0).getDate();
+
   return { firstDay, daysInMonth, Month, todayFull };
 };
