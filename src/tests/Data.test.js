fb2e4a0faf6b9a866c6703b22d4f9ad917c85abc
luckmer <woszczo04@gmail.com>
2021-05-18 16:47:24 +0200

    data test

--- src/tests/Data.test.js
+++ src/tests/Data.test.js
@@ -1,29 +1,27 @@
-import { Data } from "../views/Calendar/Data";
+import { Data } from "../views/Calendar/Data/Data";
 
 describe("Data", () => {
-  test("should", () => {});
-
-  const day = new Date();
-
   test("expect valuable data", () => {
+    const day = new Date("Tue May 18 2021 16:30:25");
     expect(Data(day)).toEqual({
       daysInMonth: 4,
       firstDay: 1,
-      hours: "10",
+      hours: "16",
       lastDay: 31,
-      minutes: "46",
+      minutes: "30",
       todayFull: "18.05.2021",
     });
   });
 
-  test("expect new content", () => {
+  test("expect new date", () => {
+    const day = new Date("Tue May 02 2014 12:30:25");
     expect(Data(day)).toEqual({
-      daysInMonth: 4,
+      daysInMonth: 2,
       firstDay: 1,
-      hours: "10",
+      hours: "12",
       lastDay: 31,
-      minutes: "46",
-      todayFull: "18.05.2021",
+      minutes: "30",
+      todayFull: "02.05.2014",
     });
   });
 });
